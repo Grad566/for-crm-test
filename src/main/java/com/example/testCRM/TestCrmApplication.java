37c96069f3d29952706b979d98f500cc4d135b00
Denis Orekhov <3008denis@gmail.com>
2024-09-18 17:10:24 +0300

    init commit

---
+++ src/main/java/com/example/testCRM/TestCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.testCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCrmApplication.class, args);
+	}
+
+}
